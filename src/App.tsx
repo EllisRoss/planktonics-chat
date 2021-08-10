b700eb83aa722fcb84dc6634676c6b29fde4ca99
EllisRoss <noumager@gmail.com>
2021-08-10 15:43:03 +0300

    add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Redirect, Route, Switch} from "react-router-dom";
 import './App.css';
+import {BusinessChat} from "./components/BusinessChat/BusinessChat";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Switch>
+          <Route exact path='/' render={() => <Redirect to='/business'/>}/>
+          <Route path='/business' render={() => <BusinessChat />}/>
+          <Route path='/communication' render={() => <div>Communication Chat Page</div>}/>
+          <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+      </Switch>
     </div>
   );
 }
