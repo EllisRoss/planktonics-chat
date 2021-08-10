4d1a5b01f10490c8164b68fb5250177c3a655b4d
EllisRoss <noumager@gmail.com>
2021-08-10 21:45:53 +0300

    add autoscroll mechanism for Messages component

--- src/components/common/Messages/Messages.tsx
+++ src/components/common/Messages/Messages.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState } from "react";
 import {Message} from "../../../types/types";
 import {MessageItem} from "./MessageItem/MessageItem";
 import styles from './Messages.module.css'
@@ -9,9 +9,28 @@ export const Messages: React.FC<MessagesListProps> = ({messages}) => {
         return <MessageItem message={m} key={m.id} />
     })
 
+    const messagesAnchorRef = useRef<HTMLDivElement>(null);
+    const [isAutoScroll, setIsAutoScroll] = useState(true);
+
+    const scrollHandler = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const element = event.currentTarget;
+        if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) > 100) {
+            isAutoScroll && setIsAutoScroll(false)
+        } else {
+            !isAutoScroll && setIsAutoScroll(true)
+        }
+    }
+
+    useEffect(() => {
+        if (isAutoScroll) {
+            messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
+        }
+    },[messages]);
+
     return (
-        <div className={styles.messages}>
+        <div className={styles.messages} onScroll={scrollHandler}>
             {messagesList}
+            <div ref={messagesAnchorRef}></div>
         </div>
     );
 }
