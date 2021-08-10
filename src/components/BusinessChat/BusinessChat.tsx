5b4d1bba1ae5626cec030f1a5509832eb8450834
EllisRoss <noumager@gmail.com>
2021-08-10 17:02:24 +0300

    add reset form to senMessage function and add divider

--- src/components/BusinessChat/BusinessChat.tsx
+++ src/components/BusinessChat/BusinessChat.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {selectBusinessMessages} from "../../redux/businessChatSelectors";
-import {Messages} from "../common/Messages";
-import {AddMessageForm, FormValues} from "../common/AddMessageForm";
+import {Messages} from "../common/Messages/Messages";
+import {AddMessageForm, FormValues} from "../common/AddMessageForm/AddMessageForm";
 import {Message} from "../../types/types";
 import {v1} from "uuid";
 import {businessChatActions} from "../../redux/businessChatReducer";
 import styles from './BusinessChat.module.css'
+import {FormikHelpers} from "formik";
+import {Divider} from "antd";
 
 export const BusinessChat: React.FC = () => {
     const messages = useSelector(selectBusinessMessages);
     const dispatch = useDispatch();
 
-    const sendMessage = (values: FormValues) => {
+    const sendMessage = (values: FormValues, {resetForm}: FormikHelpers<FormValues>) => {
         const newMessage: Message = {
             userName: 'Mark',
             message: values.msg,
@@ -21,12 +23,14 @@ export const BusinessChat: React.FC = () => {
             id: v1(),
         }
         dispatch(businessChatActions.messageAdded(newMessage))
+        resetForm({})
     }
 
     return (
         <div>
             <h2 className={styles.chat__header}>Business Chat</h2>
             <Messages messages={messages} />
+            <Divider />
             <AddMessageForm sendMessage={sendMessage}/>
         </div>
     );
