62996432d8a7405cdd217bcfe631515593d64d33
EllisRoss <noumager@gmail.com>
2021-08-10 21:38:55 +0300

    update header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,16 +6,17 @@ import styles from "./Header.module.css";
 export const Header: React.FC = () => {
     return (
         <header className={styles.header}>
+            <h1 className={styles.header__title}>Planktonics Chat</h1>
             <div className={styles.header__navbar}>
                 <Row>
-                    <Col className={styles.header__business} flex={1}>
-                        <NavLink activeClassName={styles.active} to='/business'>
-                            <b>Business chat</b>
+                    <Col className={styles.header__communication} flex={1}>
+                        <NavLink activeClassName={styles.active} to='/chat/communication'>
+                            <b>Communication chat</b>
                         </NavLink>
                     </Col>
-                    <Col className={styles.header__communication} flex={1}>
-                        <NavLink activeClassName={styles.active} to='/communication'>
-                            <b>Communication chat</b>
+                    <Col className={styles.header__business} flex={1}>
+                        <NavLink activeClassName={styles.active} to='/chat/business'>
+                            <b>Business chat</b>
                         </NavLink>
                     </Col>
                 </Row>
